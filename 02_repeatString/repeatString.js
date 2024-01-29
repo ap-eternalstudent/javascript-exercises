746ac7be3b4eb9e3c5cdab6b1b439b6c4bafa5a5
a.eternalstudent <a.eternalstudent@gmail.com>
2024-01-29 19:16:27 +0100

    complete excercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string,numberOfTimes) {
+  if (numberOfTimes >= 0) {
+    return string.repeat(numberOfTimes)
+    } else {
+      return 'ERROR'
+    }
+  }
 
 // Do not edit below this line
 module.exports = repeatString;
