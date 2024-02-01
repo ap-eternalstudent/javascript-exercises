021ed561f922b4c69aced0433e36fda2ca44159e
a.eternalstudent <a.eternalstudent@gmail.com>
2024-02-01 06:56:35 +0100

    my attempt at solving the problems up to number 7

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let stringToArray = string.split('');
+  let reversedString = [];
+  for (let i = 0; i<string.length ;i++) {
+    reversedString.push(stringToArray[stringToArray.length-1])
+    stringToArray.pop()
+  }
+  return reversedString.join('')
 };
+
+// split reverse join
+//   return string.split("").reverse().join("");
 
 // Do not edit below this line
 module.exports = reverseString;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber,secondNumber) {
+  let sum = 0;
+  if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number' || firstNumber < 0 || secondNumber < 0 ){
+    return 'ERROR'
+  } else {
+    if (firstNumber < secondNumber) {
+      for (let i = firstNumber; i <= secondNumber; i++) {
+        sum += i;
+      }
+    } else if (firstNumber > secondNumber){
+      for (let i = secondNumber; i <= firstNumber; i++) {
+        sum += i;
+      }
+    } else {
+      return 'ERROR'
+    }
+  }
+  return sum
 };
 
 // Do not edit below this line
